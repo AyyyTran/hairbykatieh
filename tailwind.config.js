2f23f1e8ce1ff5cf97cf322b034245f2dab06204
Andy Tran <AyyyTran@gmail.com>
2023-04-25 14:46:43 -0700

    updated main files added colors for theme.

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      primary: '#A68C75',
+      secondary: '#B8A391',
+      // card: '#D1D1D1',
+      // 'text-base': '#EAF2EF',
+      light: '#F6F4F1',
+    },
+    extend: {},
+  },
+  plugins: [],
+};
